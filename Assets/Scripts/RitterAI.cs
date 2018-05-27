f65d3acc696d5dcaf692f411bbcb6a3999748717
bpohlmann <benni20001025googlemail.com>
2018-05-27 21:31:10 +0200

    4

---
+++ Assets/Scripts/RitterAI.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RitterAI : MonoBehaviour {
+    private UnityEngine.AI.NavMeshAgent agent;
+    private bool dialog;
+    public Transform[] waypoints;
+    public float waypointPauseTime = 2;
+    private int currentWaypointIndex;
+    private GameObject player;
+    public bool spielerGefunden = false;
+    private bool quest = false;
+    private Text messageText;
+    
+
+    private Animator anim;
+    private int standBool;
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        anim = transform.GetComponent<Animator>();
+        standBool = Animator.StringToHash("Stehen");
+        player = GameObject.FindGameObjectWithTag("Player");
+        messageText = GameObject.FindGameObjectWithTag("Message").
+            GetComponent<Text>();	//uGUI
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (spielerGefunden == false)
+        {
+            WaypointWalk();
+        }
+        else  {
+
+            agent.SetDestination(gameObject.transform.position);
+
+            anim.SetBool(standBool, true );
+
+            
+        }
+
+
+
+    }
+
+    void WaypointWalk()
+    {
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            if (currentWaypointIndex == waypoints.Length - 1)
+                currentWaypointIndex = 0;
+            else
+                currentWaypointIndex++;
+           
+            StartCoroutine("WaypointPause", waypointPauseTime);
+        }
+        agent.SetDestination(waypoints[currentWaypointIndex].position);
+    }
+
+    IEnumerator WaypointPause(float seconds)
+    {
+        if (spielerGefunden == false)
+        {
+            float oldSpeed = agent.speed;
+            agent.speed = 0.1F;
+            yield return new WaitForSeconds(seconds);
+            agent.speed = oldSpeed;
+           
+        }
+        }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            spielerGefunden = true;
+            dialog = true;
+        }
+    }
+    private void OnGUI()
+    {
+        if (dialog)
+        {
+            if (quest == false) {
+                Time.timeScale = 0; 
+                GUILayout.BeginArea(new Rect(10, 10, 200, 200));
+                GUILayout.Label("ja/Nein");
+                if (GUILayout.Button("Ja"))
+                {
+                    quest = true;
+                    Time.timeScale = 1;
+                    dialog = false;
+
+                }
+                if (GUILayout.Button("Nein"))
+                {
+                    quest = false;
+                    Time.timeScale = 1;
+                    dialog = false;
+                }
+                GUILayout.EndArea();
+            }
+            
+        }
+    }
+    
+}
---
+++ Assets/Scripts/RitterFolgenAI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RitterFolgenAI : MonoBehaviour {
+    public float rotationsGeschwindigkeit = 5;
+    public Transform Spieler;
+    public float geschwindigkeit = 100;
+    public float minimalAbstand = 3;
+
+    private Animator anim;
+    private int standBool;
+    
+
+    // Use this for initialization
+
+    void Start () {
+        anim = transform.GetComponent<Animator>();
+        standBool = Animator.StringToHash("Stehen");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Rotieren(Spieler.position);
+        Folgen(Spieler.position );
+       
+    }
+
+    void Rotieren(Vector3 SpielerPosition)
+    {
+        //Zeitversetzt
+        Vector3 posi = SpielerPosition - transform.position;  
+        Quaternion.LookRotation(posi);
+        Quaternion destRotation = Quaternion.LookRotation(posi);
+        transform.rotation = Quaternion.Slerp(transform.rotation, destRotation, rotationsGeschwindigkeit * Time.deltaTime); 
+    }
+
+    void Folgen(Vector3 SpielerPosition)
+    {
+        Vector3 a;
+        Vector3 abstand = SpielerPosition - transform.position;
+        Vector3 richtung = transform.TransformDirection(Vector3.forward);
+
+        if (abstand.magnitude > minimalAbstand) {
+            anim.SetBool(standBool, false);
+            a = richtung.normalized * geschwindigkeit * Time.deltaTime;
+           
+        }
+        else
+        {
+            
+            a = Vector3.zero;
+            
+        }
+        if (abstand.magnitude <= minimalAbstand  )
+        {
+            anim.SetBool(standBool, true);
+        }
+        GetComponent<Rigidbody>().velocity = a; 
+    }
+}
